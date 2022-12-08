880aabe05a481a63f964faed56f47b2ffa0f89f6
mslauson <matthew@slauson.io>
2022-12-07 23:50:56 -0600

    fixed

--- redis-dev.sh
+++ redis-dev.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-kubectl port-forward --namespace redis svc/redis-master 6379:6379 & redis-cli -a $REDIS_DEV_PASSWORD
+kubectl port-forward --namespace redis svc/redis-master 6379:6379 & sleep 2 && redis-cli -a $REDIS_DEV_PASSWORD
 
 
 PID=`ps -ef | grep "kubectl port-forward --namespace redis svc/redis-master 6379:6379" | grep -v grep | awk '{print $2}'`
