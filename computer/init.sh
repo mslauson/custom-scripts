8a646b3106e998ba6918bead3cf6f07f15599a52
Matthew Slauson <matthew@slauson.io>
2023-05-29 19:34:24 -0500

    update

--- computer/init.sh
+++ computer/init.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
 mkdir ~/installs
+mkdir ~/.zsh_plugins
 mkdir -p ~/projects/{api,config,libs,ui}
 
 yay -S cava autotiling lazygit insomia ttf-jetbrains-mono-nerd toilet \
   lolcat zsh kitty neovim neofetch ranger rofi wofi fortune-mod
 
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh_plugins/zsh-autosuggestions
+
+git clone https://gitea.slauson.io/mslauson/wm-config.git ~/projects/config/wm-config
+~/projects/config/wm-config/init.sh
+
+git clone https://gitea.slauson.io/mslauson/term-config.git ~/projects/config/term-config
+~/projects/config/term-config/init.sh
+
