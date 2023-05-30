f6c0233fa286747a6da2eaed4dbd5bbfc0e15078
Matthew Slauson <matthew@slauson.io>
2023-05-29 21:14:27 -0500

    update

--- computer/init.sh
+++ computer/init.sh
@@ -5,7 +5,8 @@ mkdir ~/.zsh_plugins
 mkdir -p ~/projects/{api,config,libs,ui}
 
 yay -S cava autotiling lazygit insomia ttf-jetbrains-mono-nerd toilet \
-  lolcat zsh kitty neovim neofetch ranger rofi wofi fortune-mod
+  lolcat zsh kitty neovim neofetch ranger rofi wofi fortune-mod \
+  ripgrep autojump
 
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 
