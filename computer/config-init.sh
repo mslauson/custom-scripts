75e9cd1ad3141341d28bc453bf7abd6f879e4c68
Matthew Slauson <matthew@slauson.io>
2023-05-31 02:18:54 -0500

    update

--- computer/config-init.sh
+++ computer/config-init.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
 git -C ~/projects/config/wm-config pull
-./~/projects/config/wm-config/init.sh
-git -C ~/projects/config/term-config pull
-./~/projects/config/term-config/init.sh
-git -C ~/projects/config/astro-config pull
-./~/projects/config/astro-config/init.sh
+git -C home/mslauson/projects/config/term-config pull
+git -C home/mslauson/projects/config/astro-config pull
