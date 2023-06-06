107ed7fff2ce2ba5bd44676a36828df3fe7e4d84
Matthew Slauson <matthew@slauson.io>
2023-06-06 16:35:45 -0500

    fix

--- computer/config-init.sh
+++ computer/config-init.sh
@@ -3,5 +3,5 @@
 git -C ~/projects/config/wm-config pull
 git -C ~/installs/custom-scripts pull
 git -C ~/notes pull
-git -C home/mslauson/projects/config/term-config pull
-git -C home/mslauson/projects/config/astro-config pull
+git -C /home/mslauson/projects/config/term-config pull
+git -C /home/mslauson/projects/config/astro-config pull
