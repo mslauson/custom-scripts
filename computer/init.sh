404c727e5c429aef6f66323e71940c5fc4dd38ec
Matthew Slauson <matthew@slauson.io>
2023-06-09 11:58:58 -0500

    add

--- computer/init.sh
+++ computer/init.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
 
+echo "installing base packages..."
 mkdir ~/installs
 mkdir ~/.zsh_plugins
 mkdir -p ~/projects/{api,config,libs,ui}
 
+echo "Would you like to init nfs mounts (fstab)
+"
+##
+10.7.5.60:/mnt/general/personal/media     /mnt/media     nfs     _netdev,noauto,x-systemd.automount,x-systemd.mount-timeout=10,timeo=14,x-systemd.idle-timeout=1min 0 0
+10.7.5.60:/mnt/general/backup /mnt/backup        nfs     _netdev,noauto,x-systemd.automount,x-systemd.mount-timeout=10,timeo=14,x-systemd.idle-timeout=1min 0 0
 sudo pacman -Syyu
 
-
+echo "installing yay..."
 pacman -S --needed git base-devel
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 
+echo "updating with yay..."
+
+yay -S nerd-fonts-jetbrains-mono-160
+
 yay -S --needed --noconfirm cava autotiling lazygit insomia ttf-jetbrains-mono-nerd toilet \
   lolcat zsh kitty neovim neofetch ranger rofi wofi fortune-mod \
   ripgrep autojump hyprland waybar-hyprland-git  \
-  waybar-mpris-git python rustup kitty fish wofi \
+  waybar-mpris-git python3 rustup kitty  wofi \
   xdg-desktop-portal-hyprland-git tty-clock-git swaylockd grim slurp \
   pokemon-colorscripts-git starship jq dunst wl-clipboard swaylock-effects-git \
-  swww-git nerd-fonts-git golang aur-talk-git aurpublish yup
+  swww-git  golang aur-talk-git aurpublish yup aurvote-utils \
+  package-query
 
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 
