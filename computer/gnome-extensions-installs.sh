37e7449e7a332f4074b7c360c8a8eb27106f5a43
Matthew Slauson <matthew@slauson.io>
2023-10-21 15:02:47 -0500

    add

--- computer/gnome-extensions-installs.sh
+++ computer/gnome-extensions-installs.sh
@@ -3,7 +3,9 @@
 echo "Installing Gnome Extensions" | lolcat
 
 yay -S --needed --noconfirm \
-	gnome-shell-extension-forge gnome-shell-extension-dash-to-dock gnome-shell-extension-just-perfection-desktop
+	gnome-shell-extension-forge gnome-shell-extension-dash-to-dock \
+	gnome-shell-extension-just-perfection-desktop gnome-shell-extension-space-bar-git \
+	gnome-shell-extension-transparent-top-bar-git
 
 echo "Installing GTK Themes" | lolcat
 
